e21f4d438c261aa35428a90f659b562525017588
kamleshkalm <kamleshkalm981@gmail.com>
2025-09-25 18:45:46 +0530

    commited by author

---
+++ packages/wrapper/src/index.ts
@@ -0,0 +1,8 @@
+// Re-export everything from core
+export * from '@react-angular-fusion/core';
+
+// Re-export everything from angular adapter
+export * from '@react-angular-fusion/angular';
+
+// Re-export everything from react adapter
+export * from '@react-angular-fusion/react';
