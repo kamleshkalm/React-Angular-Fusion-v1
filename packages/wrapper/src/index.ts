3c71c79e9420c4de9084b819c2f7dcd109400fae
kamleshkalm <kamleshkalm981@gmail.com>
2025-09-25 18:51:10 +0530

    deleted

--- packages/wrapper/src/index.ts
+++
@@ -1,8 +0,0 @@
-// Re-export everything from core
-export * from '@react-angular-fusion/core';
-
-// Re-export everything from angular adapter
-export * from '@react-angular-fusion/angular';
-
-// Re-export everything from react adapter
-export * from '@react-angular-fusion/react';
